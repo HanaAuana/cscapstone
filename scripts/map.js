5206900d6420895fde7711ddc7eeaec876903d1f
Michael Lim <dlim2010@gmail.com>
2014-02-24 15:36:51 -0800

    Map now renders inside a Backbone view

--- scripts/map.js
+++ scripts/map.js
@@ -32,18 +32,18 @@ requirejs(['leaflet',
 , function(L,$, _, Backbone) {
 
 	var MapView = Backbone.View.extend({
-		el: "#mapView",
-		template: _.template( $('#map-template').html(), {}),
+		id: "mapView",
+		template: _.template( $('#map-template').html() ),
 		render: function(){
-			this.$el.append(this.template);
+			this.$el.html(this.template);
 			return this;
 		}	
 	});
-
+	
 	var mapView = new MapView();
-	mapView.render();
-    	console.log("initMap called, el is "+ mapView.el.innerHTML);
-    	var map = L.map(mapView.el).setView([47.2622639, -122.5100545], 10);
-    	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
-	mapView.render();
+	$("#appView").append(mapView.render().el);
+    	//console.log("initMap called, el is "+ mapView.el.innerHTML);
+    var map = L.map(mapView.el).setView([47.2622639, -122.5100545], 10);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+	//mapView.render();
 });
