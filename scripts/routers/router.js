e8945a36df0940cb25cf2777a17edd1988472d0f
Nathan Pastor <npastor@pugetsound.edu>
2014-03-11 23:33:24 -0700

    refactored some server logic into the router, although that's probably not where it should go

--- scripts/routers/router.js
+++ scripts/routers/router.js
@@ -2,22 +2,99 @@
  * Created by Nathan P on 1/25/14.
  */
 
-define([
-    'fs'
-], function(fs) {
+define(['scripts/utils/censusAPI',
+    'scripts/utils/googleStaticAPI'
+], function(censusAPI, googleStaticAPI) {
 
-    function route(pathname) {
-        console.log("router : Routing pathname " + pathname);
+    var completedAPIs;
 
-        // If pathname specifies a file, serve up that file synchronously
-        if(pathname.split(".").length > 1) {
-            return fs.readFileSync(".." + pathname, 'utf-8');
+    function checkCallsFinished(request, response, cityModel) {
+        for(var i = 0; i < completedAPIs.length; i++) {
+            if(!completedAPIs[i])
+                return;
+        }
+
+        var results = {
+            city: cityModel,
+            sessionID: request.sessionID
+        };
+
+        // Send back the modified body
+        response.send(JSON.stringify(results));
+        console.log('sent sim session response');
+    }
+
+    function onBoundaryResponse(cityModel, request, appResponse, res, context) {
+
+        if(res === false) {
+            console.log("boundary fails");
+            appResponse.writeHead(404);
+            appResponse.send();
+            return;
+        }
+
+        var geoObj = res.objects[0];
+        cityModel.cityName = geoObj.name;
+        console.log('setting name: ' + cityModel.cityName);
+        // swap lat/lng for consistency
+        cityModel.centroid = [geoObj.centroid.coordinates[1],
+            geoObj.centroid.coordinates[0]];
+        cityModel.stateID = geoObj.metadata.STATEFP10;
+        console.log('setting centroid: ' + cityModel.centroid.toString());
+
+        completedAPIs[0] = true;
+        checkCallsFinished(request, appResponse, cityModel)
+    }
+
+    function onTzResponse(cityModel, request, appResponse, res, context) {
+        if(res === false) {
+            console.log("tz fails");
+            appResponse.writeHead(404);
+            appResponse.send();
+            return;
+        }
+
+        // If successful, grab the timezone
+        cityModel.timezone = res.timeZoneId;
+        console.log('setting tz: ' + cityModel.timezone);
+
+        completedAPIs[1] = true;
+        checkCallsFinished(request, appResponse, cityModel);
+    }
+
+    function simSessionRoute(req, response) {
+
+        completedAPIs = [false, false];
+
+//        response.writeHead(200, {'Content-Type': 'application/json'});
+
+        var that = this;
+        // A put means this is a new model. We nee to do all the initializing
+        // stuff, including setting the city, getting all geo, and generating
+        // trips
+        if(req.route.method === 'put') {
+            console.log('handling put');
+
+            var cityModel = req.body.city;
+
+            // get the city name, boundary and centroid
+            censusAPI.getBoundaryLocation(req.body.city.location, function(res) {
+                onBoundaryResponse(cityModel, req, response, res, that)
+            }, this);
+
+            // get the timezone
+            googleStaticAPI.getTimezone(req.body.city.location, function(res) {
+                onTzResponse(cityModel, req, response, res, that);
+            }, this);
+
+        } else if(req.route.method === 'post') {
+            console.log('handle post');
         }
     }
 
     // These are the exports
     return {
-        route: route
+        simSession: simSessionRoute
     };
 
 });
