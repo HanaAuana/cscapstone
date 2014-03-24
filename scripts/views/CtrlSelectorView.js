17b113e220b2fdd703c490906d6a3cebd5841ee8
Nathan Pastor <npastor@pugetsound.edu>
2014-03-23 17:57:19 -0700

    Move CSS styles to their own file

---
+++ scripts/views/CtrlSelectorView.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Nathan P on 3/23/14.
+ */
+
+define(['jquery',
+    'underscore',
+    'backbone',
+    'tinycolor',
+    'leafletDraw',
+    'text!MapViewTemplate.ejs'
+], function ($, _, Backbone) {
+
+    var CtrlSelectorView = new Backbone.View.extend({
+
+    });
+
+    return CtrlSelectorView;
+});
