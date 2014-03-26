420344ebd41de2ea99a71a09c892528b81dfa6f9
brobiwankenobi <bkearns@pugetsound.edu>
2014-03-25 22:01:23 -0700

    Added new database script and script to query the database.

--- scripts/database/connect.js
+++ scripts/database/connect.js
@@ -1,9 +1,8 @@
 var mysql = require('mysql');
 var DATABASE = "capstone";
-var TABLE = "Cities";
-var TABLE2 = "CityPopTots";
-var TABLE3 = "StateShapes";
-
+var TABLE = "CityPops";
+//Test to see if it writes..
+var post  = {tractIDvariable:01, tractBlobVariable:20};
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -13,15 +12,20 @@ var connection = mysql.createConnection({
   port     : 8889
 });
 
-
-function getState(city, callback){
-  connection.query('select cityPopID, stateID from ' + TABLE + " where cityName = ? ", [city], function(err, result) {
-    if(err){
-      callback(err, null);
-    }
+//Need to wrap this inside a function.
+//Need to change 01 to varaible being passed in....
+connection.query('select tractBlob from ' + TABLE + ' where tractID = 01',
+function(err, result) {
+    if (err) throw err;
     else {
-        callback(null, result[0].stateID);
+        for (var i in result) {
+            var tract = result[i];
+            console.log('01 : '+ tract.tractBlob);
+        }
     }
-  });
-}
+});
+//Need to wrap this inside a function.
+//Write to the database.
+connection.query('INSERT INTO messages VALUES ?', post, function(err, result) {
 
+});
